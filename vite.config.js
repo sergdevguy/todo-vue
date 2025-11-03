7cad885e505f12430f5fedd5d0a3a0bb3c9e285e
Sergey Harchenko <sergdevguy@gmail.com>
2025-11-03 19:34:32 +0300

    create completed MVP

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, 'src', 'components')
+    }
+  }
 })
